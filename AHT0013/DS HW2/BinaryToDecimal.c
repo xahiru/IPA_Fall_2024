a10c85e76fb6a30e081a6cafd79d66a6685854a5
tuhin758 <ahtuhin758@gmail.com>
2024-09-27 10:56:36 +0800

    BinaryToDecimal

---
+++ AHT0013/DS HW2/BinaryToDecimal.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+
+int converter(long long n) {
+    int decimalNum = 0,i = 1,remainder;
+
+
+    while (n > 0) {
+        remainder = n % 10;
+        dicimalNum += remainder * i;
+        i *= 2;
+        n /= 10;
+    }
+    return decimalNum;
+}
+
+int main() {
+    long long binaryNum;
+
+
+    printf(*Enter a binary number: *);
+    scanf("%lld" , &binaryNum);
+
+
+    printf("Decimal : %d\n", converter(binaryNum));
+
+    return 0;
+}
