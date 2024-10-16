1a7f3b475341f5cce28a4705c794e6e81d7dd7ac
Ahmed Zahir <xahiru@gmail.com>
2024-10-16 18:46:12 +0800

    vue-temple-syntax-demo app added

---
+++ VUE_APP/vue-temple-syntax-demo/src/components/EventDemo.vue
@@ -0,0 +1,29 @@
+<script setup>
+const handleClick = () => {
+  alert('Button clicked!');
+};
+</script>
+
+<template>
+  <div>
+    <h2>Event Modifiers: Stop Propagation</h2>
+    <div @click="() => alert('Div clicked!')" style="border: 2px solid #ccc; padding: 20px;">
+      <button @click.stop="handleClick">Click me without triggering div click</button>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+div {
+  margin: 20px 0;
+}
+
+button {
+  background-color: #42b983;
+  color: white;
+  padding: 5px 10px;
+  border: none;
+  border-radius: 3px;
+  cursor: pointer;
+}
+</style>
