16acf53e0f8c7e34e243cb184775e58da0e57bca
tuhin758 <ahtuhin758@gmail.com>
2024-10-24 15:24:18 +0800

    ipa

---
+++ AHT0013/Mid-term_assignment_project/mid-term-assignment/src/App.vue
@@ -0,0 +1,27 @@
+<script setup>
+import ToggleVisibility from './components/ToggleVisibility.vue';
+import { ref } from 'vue'
+
+const isActive = ref(true);
+const handleClick=()=>{
+  if(isActive.value==true){
+    isActive.value=false
+  }
+  else{
+    isActive.value=true
+  }
+}
+</script>
+
+<template>
+  <div>
+    <button @click="handleClick">BTN</button>
+
+  <div v-if="isActive">
+    <ToggleVisibility />
+  </div>
+</div>
+</template>
+
+<style scoped>
+</style>
