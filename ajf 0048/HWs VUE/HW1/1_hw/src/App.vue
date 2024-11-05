42af9435fafde13ee04a00a0f537f1f4803a7de8
Your Name <luo@DESKTOP-08A660E>
2024-11-05 15:14:24 +0800

    HW1

---
+++ ajf 0048/HWs VUE/HW1/1_hw/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <LoginForm />
+  </div>
+</template>
+
+<script>
+import LoginForm from './components/LoginForm.vue'
+
+export default {
+  name: 'App',
+  components: {
+    LoginForm
+  }
+}
+</script>
