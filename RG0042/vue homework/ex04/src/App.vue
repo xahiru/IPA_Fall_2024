6437b5cbd57d95d844dca8b09816d2d08b931dd3
Rabby0501 <golamrabby213@gmail.com>
2024-11-05 18:39:51 +0600

    vue homework

--- RG0042/vue homework/ex04/src/App.vue
+++ RG0042/vue homework/ex04/src/App.vue
@@ -1,30 +1,33 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div id="app">
+    <h1>Vue 3 Conditional Rendering Example</h1>
+    <ToggleVisibility />
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+<script>
+import ToggleVisibility from './components/ToggleVisibility.vue';
+
+export default {
+  name: 'App',
+  components: {
+    ToggleVisibility
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #ae0e0e;
+  margin-top: 60px;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+
+h1 {
+  font-size: 2em;
+  margin-bottom: 20px;
 }
 </style>
