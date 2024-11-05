29fd28d4feb43d2b33e3470f60abb1b473d4ddc3
tuhin758 <ahtuhin758@gmail.com>
2024-11-05 16:32:42 +0800

    4th HW

--- AHT0013/Mid-term_assignment_project/greenhouse-project/src/components/HelloWorld.vue
+++ AHT0013/Mid-term_assignment_project/greenhouse-project/src/components/HelloWorld.vue
@@ -38,6 +38,6 @@ const count = ref(0)
 
 <style scoped>
 .read-the-docs {
-  color: #005835;
+  color: #888;
 }
 </style>
---
+++ AHT0013/Vue HW/HW2/src/components/EventDemo.vue
@@ -0,0 +1,23 @@
+<template>
+    <div @click="handleDivClick">
+      <p></p>
+      <button @click.stop="handleButtonClick">Click me</button>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'EventDemo',
+    methods: {
+      handleDivClick() {
+        alert('Div clicked!');
+      },
+      handleButtonClick() {
+        alert('Button clicked!');
+      },
+    },
+  };
+  </script>
+  
+  <style scoped>
+  </style>
