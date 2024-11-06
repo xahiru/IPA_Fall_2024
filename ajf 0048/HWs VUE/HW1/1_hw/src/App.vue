67b4041c36655545e4fd188c24c563355b70f2c4
Your Name <luo@DESKTOP-08A660E>
2024-11-06 12:30:22 +0800

    3_hw

--- ajf 0048/HWs VUE/HW1/1_hw/src/App.vue
+++ ajf 0048/HWs VUE/HW1/1_hw/src/App.vue
@@ -2,18 +2,21 @@
   <div id="app">
     <LoginForm />
     <EventDemo />
+    <ExpressionDemo />
   </div>
 </template>
 
 <script>
 import LoginForm from './components/LoginForm.vue'
 import EventDemo from './components/EventDemo.vue'
+import ExpressionDemo from './components/ExpressionDemo.Vue'
 
 export default {
   name: 'App',
   components: {
     LoginForm,
-    EventDemo
+    EventDemo,
+    ExpressionDemo
   }
 }
 </script>
