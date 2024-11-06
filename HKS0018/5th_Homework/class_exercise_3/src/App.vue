60a42759b94ce866788e750862e36a4c268528b8
shimanto1025 <kazishimant069@gmail.com>
2024-11-06 20:02:01 +0800

    class exercise 3 completed

---
+++ HKS0018/5th_Homework/class_exercise_3/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <ExpressionDemo />
+  </div>
+</template>
+
+<script>
+import ExpressionDemo from './components/ExpressionDemo.vue';
+
+export default {
+  name: 'App',
+  components: {
+    ExpressionDemo
+  }
+};
+</script>
+
+<style>
+
+#app {
+  text-align: center;
+  margin-top: 40px;
+}
+</style>
