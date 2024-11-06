18fc8fee9f4c6685c35af901ab0beab463c8b11c
shimanto1025 <kazishimant069@gmail.com>
2024-11-06 17:20:10 +0800

    5th homework execrcise 1 complete

---
+++ HKS0018/5th_Homework/Class_exercise_1/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <login-form />
+  </div>
+</template>
+
+<script>
+import LoginForm from './components/LoginForm.vue';
+
+export default {
+  name: 'App',
+  components: {
+    LoginForm
+  }
+};
+</script>
+
+<style>
+
+body{
+  background:white;
+}
+#app {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  margin: 100;
+  background: linear-gradient(135deg, #4b79a1, #283e51);
+  font-family: Arial, sans-serif;
+}
+</style>
