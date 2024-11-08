1335aec87002ae4832434b75ebba5b2e2ffd4aa4
Rabby0501 <golamrabby213@gmail.com>
2024-11-09 03:41:25 +0600

    Add App file

--- RG0042/assignment project/greenhouse-monitoring-system/src/App.vue
+++ RG0042/assignment project/greenhouse-monitoring-system/src/App.vue
@@ -1,30 +1,89 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div id="app">
+    <nav>
+      <router-link to="/" class="nav-link">Home</router-link>
+      <router-link to="/loginPage" class="nav-link">login</router-link>
+      <router-link to="/signuppage" class="nav-link">Sign Up</router-link>
+
+      <button v-if="isAuthenticated" @click="logout" class="nav-link logout-btn">logout</button>
+    </nav>
+
+    <main>
+      <router-view></router-view>
+    </main>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      isAuthenticated: false,
+    };
+  },
+
+  created() {
+    this.checkAutj();
+  },
+
+  methods: {
+    checkAuth() {
+      // check local storage to see if user is authenticated
+      this.isAuthenticated = !!localStorage.getItem("auth");
+    },
+    logout() {
+      localStorage.removeItem("auth");
+      this.isAuthenticated = false;
+      this.$router.push("/login");  //redirect to login page
+    },
+  },
+
+  watch: {
+    // watch route change and update authenticated status
+    $router() {
+      this.checkAuth();
+    }
+  }
+};
+</script>
+
+
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+#app {
+  font-family: Arial, Helvetica, sans-serif;
+  text-align: center;
+  color: darkviolet;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+
+nav {
+  display: flex;
+  justify-content: center;
+  padding: 15px;
+  background-color: blueviolet;
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+
+
+.nav-link {
+  margin: 0 10px;
+  color: white;
+  text-decoration: none;
+  font-weight: bold;
+}
+
+.nav-link:hover {
+  text-decoration: underline;
+}
+
+.logout-btn {
+  background: none;
+  border: none;
+  color: white;
+  font-weight: bold;
+  cursor: pointer;
+}
+
+main {
+  padding: 20px;
 }
 </style>
