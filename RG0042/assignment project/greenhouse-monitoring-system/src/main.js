e1e8fd19fc886c31096f4cec47a480e6ce2d1eed
Rabby0501 <golamrabby213@gmail.com>
2024-11-12 17:17:31 +0600

    make chenge

--- RG0042/assignment project/greenhouse-monitoring-system/src/main.js
+++ RG0042/assignment project/greenhouse-monitoring-system/src/main.js
@@ -1,12 +1,13 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
-import store from './store';
+import { createPinia } from 'pinia';
 
 
 const app = createApp(App);
+const pinia = createPinia();
 
 app.use(router);
-app.use(store);
+app.use(pinia);
 
 app.mount('#app');
