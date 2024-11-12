402e1fbacb6dc6294dce71db224989f14a5b251a
Rabby0501 <golamrabby213@gmail.com>
2024-11-12 21:06:22 +0600

    add Navbar in components

---
+++ RG0042/assignment project/greenhouse-monitoring-system/src/components/Navbar.vue
@@ -0,0 +1,45 @@
+<template>
+    <nav class="navbar">
+        <router-link to="/" class="logo">Greenhouse Dashboard</router-link>
+        <div class="nav-links">
+            <router-link to="/overview">Overview</router-link>
+            <router-link to="/settings">Settings</router-link>
+            <router-link to="/logs">Logs</router-link>
+            <router-link to="/login">Logout</router-link>
+        </div>
+    </nav>
+</template>
+
+<script>
+export default {
+    name: "navbar",
+}
+</script>
+
+<style scoped>
+.navbar {
+    display: flex;
+    justify-content: space-between;
+    align-content: center;
+    padding: 1rem;
+    background-color: white;
+}
+
+.logs {
+    font-size: 1.5rem;
+    font-weight: bold;
+    color: white;
+    text-decoration: none;
+
+}
+
+.nav-links {
+    margin: 0 1rem;
+    color: white;
+    text-decoration: none;
+}
+
+.nav-links a:hover {
+    text-decoration: underline;
+}
+</style>
