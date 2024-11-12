8a509b9142b9b09709deb315cdc283fa1023672f
Rabby0501 <golamrabby213@gmail.com>
2024-11-13 05:45:09 +0600

    add sidebar

---
+++ RG0042/assignment project/greenhouse-monitoring-system/src/components/Sidebar.vue
@@ -0,0 +1,43 @@
+<template>
+    <aside class="sidebar">
+      <ul>
+        <li><router-link to="/overview">Overview</router-link></li>
+        <li><router-link to="/settings">Settings</router-link></li>
+        <li><router-link to="/logs">Logs</router-link></li>
+        
+      </ul>
+    </aside>
+  </template>
+  
+  <script>
+  export default {
+    name: "Sidebar",
+  };
+  </script>
+  
+  <style scoped>
+  .sidebar {
+    background-color: #f3f4f6;
+    padding: 1rem;
+    width: 250px;
+  }
+  
+  .sidebar ul {
+    list-style: none;
+    padding: 0;
+  }
+  
+  .sidebar li {
+    margin: 1rem 0;
+  }
+  
+  .sidebar a {
+    color: #3b82f6;
+    text-decoration: none;
+    font-weight: bold;
+  }
+  
+  .sidebar a:hover {
+    text-decoration: underline;
+  }
+  </style>
--- RG0042/assignment project/greenhouse-monitoring-system/vite.config.js
+++ RG0042/assignment project/greenhouse-monitoring-system/vite.config.js
@@ -1,9 +1,14 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
 import { fileURLToPath, URL } from 'node:url';
 
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import vueDevTools from 'vite-plugin-vue-devtools'
+
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    vueDevTools(),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
