9218e97d6d45ce13a37c4872c92e3cf8a1155951
Ashiliny <lasangaashiliny@gmqil.com>
2024-11-13 03:57:27 -0800

    First

--- NS0064/homework5/src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import './style.css';
-import router from '../src/router/index';
-
-const app = createApp(App);
-app.use(router);
-app.mount('#app');
