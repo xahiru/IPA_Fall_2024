9d62a42e62e91187b03330734c59f40a10236f76
Rabby0501 <golamrabby213@gmail.com>
2024-11-20 10:57:18 +0600

    Update Home

--- RG0042/assignment project/greenhouse-monitoring-system/src/views/Home.vue
+++ RG0042/assignment project/greenhouse-monitoring-system/src/views/Home.vue
@@ -6,22 +6,18 @@
       <p>Monitor and manage your Greenhouse environment easily</p>
     </header>
 
-    <section>
       <div class="auth-buttons">
       <router-link to="/login">
         <button>Login</button></router-link>
       <router-link to="/signup">
         <button>Sign Up</button></router-link>
         </div>
-    </section> 
     </div>
     </div>
 </template>
   
   
 <script>
-
- 
     export default {
     name: "Home",
 };
@@ -34,10 +30,8 @@
     background-position: center;
     background-repeat: no-repeat;
     height: 100vh;
-    
     display: flex;
-    flex-direction: column;
-    text-align: center;
+    justify-content: center;
     align-items: center;
 }
 
@@ -50,17 +44,19 @@
     width: 100%;
     max-width: 350px;
 }
+
 h1 {
-    text-align: center;
     color: white;
     font-family: 'Times New Roman', Times, serif;
-}
-p {
-    text-align: center;
-    color: black;
+    margin-bottom: 1rem;
 }
 
-.auth-buttons{
+p {
+    color: rgba(255, 255, 255, 0.8);
+    margin-bottom: 2rem;
+}
+
+.auth-buttons {
     display: flex;
     flex-direction: column;
     gap: 1rem;
@@ -73,22 +69,24 @@ button {
     padding: 0.75rem 1.5rem;
     font-size: 1rem;
     cursor: pointer;
-    align-items: center;
     text-align: center;
     color: rgb(251, 248, 255);
     background-color: rgb(117, 212, 141);
     border-radius: 5px;
+    border: none;
+
     width: 100%;
     max-width: 200px;
     height: 50px;
 }
 
 button:hover {
-    background-color: rgb(87, 162, 105);;
+    background-color: rgb(87, 162, 105);
 }
 
+
 @media (max-width: 768px) {
-    .content-page{
+    .content-page {
         padding: 15px;
     }
     h1 {
@@ -97,7 +95,6 @@ button:hover {
     p {
         font-size: 1rem;
     }
-
     button {
         width: 100%;
         font-size: 1.2rem;
@@ -111,7 +108,6 @@ button:hover {
     p {
        font-size: 0.9rem; 
     }
-
     button {
         font-size: 1rem;
     }
