f80d8379df1d2431a205a0434bf1e72e3f59a7d4
Rabby0501 <golamrabby213@gmail.com>
2024-11-21 23:46:59 +0600

    impont components in router

--- RG0042/assignment project/greenhouse-monitoring-system/src/router.js
+++ RG0042/assignment project/greenhouse-monitoring-system/src/router.js
@@ -3,12 +3,14 @@ import Home from "./views/Home.vue";
 import Login from "./views/Login.vue";
 import Signup from "./views/Signup.vue";
 import Dashboard from "./views/Dashboard.vue";
+import SettingsPanel from "./components/SettingsPanel.vue";
 
 const routes = [
     {path: '/', component: Home},
     {path: '/login', component: Login},
     {path: '/signup', component: Signup},
     {path: '/dashboard', component: Dashboard, meta: {reqiresAuth: true} },
+    {path: '/SettingsPanel', component: SettingsPanel, meta: {requiresAuth: true}},
 ];
 
 const router = createRouter({
