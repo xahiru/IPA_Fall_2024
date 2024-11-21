85a2f97b502170280c9e9aeb18ad2c9615521818
Rabby0501 <golamrabby213@gmail.com>
2024-11-21 22:31:43 +0600

    again update Navbar

--- RG0042/assignment project/greenhouse-monitoring-system/src/components/Navbar.vue
+++ RG0042/assignment project/greenhouse-monitoring-system/src/components/Navbar.vue
@@ -1,53 +1,52 @@
 <template>
     <nav class="navbar">
-        <router-link to="/" class="logo">Greenhouse Dashboard</router-link>
-        <div class="nav-links">
-            <router-link to="/overview">Overview</router-link>
-            <router-link to="/settings">Settings</router-link>
-            <router-link to="/logs">Logs</router-link>
-            <router-link to="/login">Logout</router-link>
-        </div>
+      <router-link to="/" class="logo">Greenhouse Dashboard</router-link>
+      <div class="nav-links">
+        <router-link to="/overview">Overview</router-link>
+        <router-link to="/settingspanel">Settings</router-link>
+        <router-link to="/logs">Logs</router-link>
+        <router-link to="/login">Logout</router-link>
+      </div>
     </nav>
-</template>
-
-<script>
-export default {
-    name: "navbar",
-
+  </template>
+  
+  <script>
+  export default {
+    name: "Navbar",
+  
     methods: {
-        logout() {
-        localStorage.removeItem('isAuthenticated');
-        this.$router.push('/login');    
+      logout() {
+        localStorage.removeItem('isAuthenticated'); // Remove the auth flag
+        this.$router.push('/login'); // Redirect to login page
+      }
     }
-  }
-};
-</script>
-
-<style scoped>
-.navbar {
+  };
+  </script>
+  
+  <style scoped>
+  .navbar {
     display: flex;
     justify-content: space-between;
-    align-content: center;
+    align-items: center;
     padding: 1rem;
     background-color: #3b82f6;
     color: white;
-}
-
-.logs {
+  }
+  
+  .logo {
     font-size: 1.5rem;
     font-weight: bold;
     color: white;
     text-decoration: none;
-
-}
-
-.nav-links {
+  }
+  
+  .nav-links a {
     margin: 0 1rem;
     color: white;
     text-decoration: none;
-}
-
-.nav-links a:hover {
+  }
+  
+  .nav-links a:hover {
     text-decoration: underline;
-}
-</style>
+  }
+  </style>
