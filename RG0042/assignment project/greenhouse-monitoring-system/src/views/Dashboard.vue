84ff0c7c653cecd4146c78b010795fa184bda46a
Rabby0501 <golamrabby213@gmail.com>
2024-11-25 22:00:34 +0600

    updated Dascboard

--- RG0042/assignment project/greenhouse-monitoring-system/src/views/Dashboard.vue
+++ RG0042/assignment project/greenhouse-monitoring-system/src/views/Dashboard.vue
@@ -11,15 +11,12 @@
   <script>
   import Navbar from "@/components/Navbar.vue";
   import DashboardContent from "@/components/DashboardContent.vue";
-  import DataCard from "@/components/DataCard.vue";
-  import SettingsPanel from "@/components/SettingsPanel.vue";
+
   export default {
     name: "Dashboard",
     components: {
       Navbar,
       DashboardContent,
-      DataCard,
-      SettingsPanel,
     },
   };
   </script>
