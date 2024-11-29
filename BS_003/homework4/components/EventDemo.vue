886f19b20bb9103f5f093a93a0aafa2ddc108841
Bonnie11117 <148852096+Bonnie11117@users.noreply.github.com>
2024-11-29 20:35:24 +0800

    Create EventDemo.vue

---
+++ BS_003/homework4/components/EventDemo.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h2>Event Handling Demo</h2>
+
+    <div @click="handleDivClick">
+      <p>Click anywhere inside the div, but outside the button, to trigger the div's click event.</p>
+      <button @click.stop="handleButtonClick">Click Me (Button)</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'EventDemo',
+  methods: {
+    handleDivClick() {
+      alert('Div clicked!');
+    },
+    handleButtonClick() {
+      alert('Button clicked!');
+    }
+  }
+};
+</script>
