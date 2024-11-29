d3b8b45218b362f9c486690c918c3ff1102e6a35
Bonnie11117 <148852096+Bonnie11117@users.noreply.github.com>
2024-11-29 20:34:00 +0800

    Create LoginForm.vue

---
+++ BS_003/homework4/components/LoginForm.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <h2>Login Form</h2>
+    <form @submit.prevent="submitForm">
+      <label for="username">Username:</label>
+      <input type="text" id="username" v-model="username" required />
+
+      <label for="password">Password:</label>
+      <input type="password" id="password" v-model="password" required />
+
+      <button type="submit">Submit</button>
+    </form>
+
+    <p v-if="welcomeMessage">Welcome, {{ welcomeMessage }}!</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LoginForm',
+  data() {
+    return {
+      username: '',
+      password: '',
+      welcomeMessage: ''
+    };
+  },
+  methods: {
+    submitForm() {
+      this.welcomeMessage = this.username;
+      alert(Welcome, ${this.username}!);
+    }
+  }
+};
+</script> humanzize
