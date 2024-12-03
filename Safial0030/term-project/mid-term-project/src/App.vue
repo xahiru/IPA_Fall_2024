2c167b949450288692448e63a6853a62a6ba7940
SafialMujnabin <swesafial131@gmail.com>
2024-12-03 22:21:44 +0800

    add temperature chart and modify app.vue

--- Safial0030/term-project/mid-term-project/src/App.vue
+++ Safial0030/term-project/mid-term-project/src/App.vue
@@ -1,26 +1,36 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <Navbar v-if="showNavbar" :bgColor="'green'" />
+    <router-view />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Navbar from './components/Navbar.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    Navbar,
+  },
+  computed: {
+    showNavbar() {
+      const isAuthenticated = localStorage.getItem('token');
+      return (
+        (this.$route.path === '/dashboard' ||
+          this.$route.path === '/settings' ||
+          this.$route.path === '/temperature-chart') &&
+        isAuthenticated
+      );
+    },
+  },
+};
 </script>
 
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 0px;
 }
 </style>
