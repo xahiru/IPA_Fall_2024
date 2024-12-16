daf075907dbdf076c2cf7ac8317e5d9605a7af96
Rabby0501 <golamrabby213@gmail.com>
2024-12-16 12:10:43 +0600

    HW_02

--- RG0042/django_homework/first_django_app/myapp/views.py
+++ RG0042/django_homework/first_django_app/myapp/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
 
-# Create your views here.
+
+def myapp(request):
+  template = loader.get_template('myfirst.html')
+  return HttpResponse(template.render())
