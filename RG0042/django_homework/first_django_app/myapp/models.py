fe91b983a7f0ae1a205419e1baec35e1257218ee
Rabby0501 <golamrabby213@gmail.com>
2024-12-18 07:22:47 +0600

    delete some file

--- RG0042/django_homework/first_django_app/myapp/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-class myapp(models.Model):
-  firstname = models.CharField(max_length=255)
-  lastname = models.CharField(max_length=255)
-  phone = models.IntegerField(null=True)
-  joined_date = models.DateField(null=True)
--- RG0042/django_homework/first_django_app/myapp/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.template import loader
-
-
-def myapp(request):
-  template = loader.get_template('myfirst.html')
-  return HttpResponse(template.render())
