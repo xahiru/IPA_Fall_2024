462afd1b77138a218c1a6477a3c0e14cfeb092a5
Fahadbcd17 <fahadbcd17@gmail.com>
2024-12-21 14:56:41 +0800

    form && generic views

--- FOG0002/django_homeWork/classWork/base/urls.py
+++ FOG0002/django_homeWork/classWork/base/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [ 
     path('articles/', views.list_articles, name='list_articles'),  
+    path('articles/create/', views.create_article, name='create_article'),
 ]
--- FOG0002/django_homeWork/classWork/base/views.py
+++ FOG0002/django_homeWork/classWork/base/views.py
@@ -1,7 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Article
+from .forms import ArticleForm
 
+# views.py
 def list_articles(request):
     articles = Article.objects.all()
-    context = {'articles': articles}
+    if request.method == 'POST':
+        form = ArticleForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('list_articles')
+    else:
+        form = ArticleForm()
+    context = {
+        'articles': articles,
+        'form': form
+    }
     return render(request, 'base/list_articles.html', context)
+
+
+def create_article(request):
+    if request.method == 'POST':
+        form = ArticleForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('list_articles')
+    else:
+        form = ArticleForm()
+    return render(request, 'base/create_article.html', {'form': form})
