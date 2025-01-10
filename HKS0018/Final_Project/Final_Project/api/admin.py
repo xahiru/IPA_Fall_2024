4465c269845931b190ce2889b267105a3f18cce7
shimanto1025 <kazishimant069@gmail.com>
2025-01-10 21:18:07 +0800

    Final_Project complete

--- HKS0018/Final_Project/Final_Project/api/admin.py
+++ HKS0018/Final_Project/Final_Project/api/admin.py
@@ -1,3 +1,7 @@
-from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from .models import GreenhouseMetrics, UserSettings
+
+
+admin.site.register(GreenhouseMetrics)
+#admin.site.register(UserSettings)
