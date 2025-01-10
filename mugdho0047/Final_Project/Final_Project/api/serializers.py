047f2d79757395354f09961bd95ed1a9e05a0f50
MugdhoYNU <mehedihasanynu@gmail.com>
2025-01-10 14:33:00 +0800

    serializers

---
+++ mugdho0047/Final_Project/Final_Project/api/serializers.py
@@ -0,0 +1,22 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers
+
+
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password']
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password']
+        )
+        return user
+    
+class UserLoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField()
