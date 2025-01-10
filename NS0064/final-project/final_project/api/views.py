ce967c2ff703c6cdca1dbb887279cdc27b613a9a
nusrat-suraia <suraiyanusrat1111@gmail.com>
2025-01-10 19:38:49 +0800

    create virews for the data and setting

--- NS0064/final-project/final_project/api/views.py
+++ NS0064/final-project/final_project/api/views.py
@@ -5,6 +5,12 @@ from .serializers import UserRegistrationSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.permissions import AllowAny
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework import permissions, viewsets
+from rest_framework.decorators import action
+from .models import GreenhouseData, UserControl
+from .serializers import UserRegistrationSerializer, GreenhouseDataSerializer, UserControlSerializer
+
 
 class UserRegistrationView(APIView):
     permission_classes = [AllowAny]
@@ -19,15 +25,35 @@ class UserRegistrationView(APIView):
 class LoginView(TokenObtainPairView):
     permission_classes = [AllowAny] 
 
-    #permission_classes = [IsAuthenticated]
 
 class LogoutView(APIView):
-    permission_classes = [IsAuthenticated]  # Ensure the user is authenticated
+    permission_classes = [IsAuthenticated]  
 
     def post(self, request):
         try:
-            # Delete the user's token
             request.user.auth_token.delete()
             return Response({"message": "Successfully logged out"}, status=status.HTTP_200_OK)
         except Exception as e:
             return Response({"detail": str(e)}, status=status.HTTP_400_BAD_REQUEST)
+
+class GreenhouseMetricsViewSet(viewsets.ModelViewSet):
+    queryset = GreenhouseData.objects.all()
+    serializer_class = GreenhouseDataSerializer
+    permission_classes = [IsAuthenticated]
+
+    # Optional action for additional functionality (e.g., filtering, or custom methods)
+    @action(detail=False, methods=['get'])
+    def latest(self, request):
+        latest_metrics = GreenhouseData.objects.last()
+        serializer = self.get_serializer(latest_metrics)
+        return Response(serializer.data)
+
+
+class UserSettingsViewSet(viewsets.ModelViewSet):
+    queryset = UserControl.objects.all()
+    serializer_class = UserControlSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_object(self):
+        # Return settings for the currently authenticated user
+        return UserControl.objects.get(user=self.request.user)
