8d93f414ae6b4effcf5d56e66c1b5d7e2f372955
MugdhoYNU <mehedihasanynu@gmail.com>
2025-01-10 18:54:27 +0800

    add models in admin site

--- mugdho0047/Final_Project/Final_Project/Final_Project/settings.py
+++ mugdho0047/Final_Project/Final_Project/Final_Project/settings.py
@@ -55,7 +55,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'final_project.urls'
+ROOT_URLCONF = 'Final_Project.urls'
 
 TEMPLATES = [
     {
@@ -73,7 +73,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'final_project.wsgi.application'
+WSGI_APPLICATION = 'Final_Project.wsgi.application'
 
 
 # Database
